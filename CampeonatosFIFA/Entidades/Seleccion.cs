ce33ab6330237e4bc1eebdaf07f296749febd93d
Broncos225 <andres.yepes419@pascualbravo.edu.co>
2024-02-12 07:26:07 -0500

    first commit

---
+++ CampeonatosFIFA/Entidades/Seleccion.cs
@@ -0,0 +1,15 @@
+﻿namespace CampeonatosFIFA.Entidades
+{
+    [Table("Pais")]
+    public class Seleccion
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Pais"), StringLength(100)]
+        public required String Nombre { get; set; }
+
+        [Column("Entidad"), StringLength(100)]
+        public required String Entidad { get; set; }
+    }
+}
